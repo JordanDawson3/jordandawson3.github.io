b0ba4624e502940b394f8da6aeb6907567664c32
Jordan Dawson <jtdawson333@gmail.com>
2024-10-22 16:37:09 -0700

    assn 5 complete

--- Assignment 5/fancifymytext.js
+++ Assignment 5/fancifymytext.js
@@ -1,3 +1,25 @@
 function bigger (){
-    alert("Hello, world!");
+    document.getElementById("textbox").style.fontSize = "24px";
+}
+
+function fancy(){
+    document.getElementById("textbox").style.fontWeight = "bold";
+    document.getElementById("textbox").style.color = "blue";
+    document.getElementById("textbox").style.textDecoration = "underline";
+}
+
+function boring(){
+    document.getElementById("textbox").style.fontWeight = "normal";
+    document.getElementById("textbox").style.color = "black";
+    document.getElementById("textbox").style.textDecoration = "none";
+}
+
+function moo(){
+    const textarea = document.getElementById("textbox");
+    var value = textarea.value;
+    value = value.toUpperCase();
+    var parts = value.split(".");
+    value = parts.join("-Moo");
+    textarea.value = value;
+
 }
